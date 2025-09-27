4857e1675afa8e6e98e53fd898c03a9b4508d3b9
Ayomide <adefeyisayo998@gmail.com>
2025-09-27 20:06:09 +0100

    Update ScrollMenu component to use anchor links for internal navigation, replacing previous page links for Services, Portfolio, Resume, Pricing, and Contact. This enhances user experience by allowing smooth scrolling to sections within the page.

--- nextjs-portfolio/src/components/header/ScrollMenu.tsx
+++ nextjs-portfolio/src/components/header/ScrollMenu.tsx
@@ -9,22 +9,22 @@ const ScrollMenu: React.FC<DataType> = ({ closeMenu }) => {
     return (
         <>
             <li>
-                <NextLink href="/services" className="smooth-menu" onClick={closeMenu}>Services</NextLink>
+                <NextLink href="#services" className="smooth-menu" onClick={closeMenu}>Services</NextLink>
             </li>
             <li>
-                <NextLink href="/projects" className="smooth-menu" onClick={closeMenu}>Portfolio</NextLink>
+                <NextLink href="#portfolio" className="smooth-menu" onClick={closeMenu}>Portfolio</NextLink>
             </li>
-            {/* <li>
-                <NextLink href="/resume" className="smooth-menu" onClick={closeMenu}>Resume</NextLink>
-            </li> */}
+            <li>
+                <NextLink href="#resume" className="smooth-menu" onClick={closeMenu}>Resume</NextLink>
+            </li>
             <li>
                 <NextLink href="/blog" className="smooth-menu" onClick={closeMenu}>Blog</NextLink>
             </li>
-            {/* <li>
-                <NextLink href="/pricing" className="smooth-menu" onClick={closeMenu}>Pricing</NextLink>
-            </li> */}
             <li>
-                <NextLink href="/contact" className="smooth-menu" onClick={closeMenu}>Contact</NextLink>
+                <NextLink href="#pricing" className="smooth-menu" onClick={closeMenu}>Pricing</NextLink>
+            </li>
+            <li>
+                <NextLink href="#contact" className="smooth-menu" onClick={closeMenu}>Contact</NextLink>
             </li>
         </>
     );
